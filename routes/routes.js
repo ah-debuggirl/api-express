43a6e287686376ec410a9e638bfa4a61650c03d0
ah-debuggirl <srita.godinez.org@gmail.com>
2018-07-18 00:12:15 -0500

    Initial commit

---
+++ routes/routes.js
@@ -0,0 +1,36 @@
+var faker =require ("faker");
+
+var appRouter =function (app){
+    app.get("/", function (req , res) {
+        res.status(200).send("Bienvenido a nuestra Restful API");
+    });
+app.get("/user",function(req, res){
+    var data= ({
+        firstName:faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email()
+    });
+    res.status(200).send(data);
+});
+    app.get("/users/:num",function (req ,res)   {
+        var users=[];
+        var num =req.params.num;
+        if(isFinite(num) && num > 0){
+            for(i=0;i<=num-1;i++){
+                users.push({
+                    firstName:faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    username: faker.internet.userName(),
+                    email: faker.internet.email()   
+                });
+            }
+        res.status(200).send(users);
+        }
+        else{
+            res.status(400).send({message:'número invalido'})
+        }
+    });
+}
+ 
+module.exports=appRouter;
